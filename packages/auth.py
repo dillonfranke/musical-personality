6f15cb99c8f656e0f43a085ce04789940adfbeb0
Dillon Reece Franke <dfranke@stanford.edu>
2019-12-02 11:36:52 -0800

    adding loading page

--- packages/auth.py
+++ packages/auth.py
@@ -11,6 +11,10 @@ from packages.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
+@bp.route('/loading')
+def loading():
+    render_template('loading')
+
 
 @bp.route('/link')
 def link():
@@ -20,7 +24,7 @@ def link():
     url += '?client_id=fef838e843a9476fa2c5c874476662fc'
     url += '&response_type=code'
     #url += '&redirect_uri=http://127.0.0.1:5000/auth/login'
-    url += '&redirect_uri=http://musicmerge.dillonfranke.com/auth/login'
+    url += '&redirect_uri=http://musicmerge.dillonfranke.com/auth/loading'
     url += quote('&scope=user-top-read user-library-read playlist-read-private playlist-read-collaborative playlist-modify-public playlist-modify-private', safe='&=-')
     #add state parameter here to prevent CSRF
     return redirect(url)
@@ -35,7 +39,7 @@ def getAccessToken(auth_code):
         'grant_type': 'authorization_code',
         'code': auth_code,
         # 'redirect_uri': 'http://127.0.0.1:5000/auth/login',
-        'redirect_uri': 'http://musicmerge.dillonfranke.com/auth/login',
+        'redirect_uri': 'http://musicmerge.dillonfranke.com/auth/loading',
         'client_id': 'fef838e843a9476fa2c5c874476662fc',
         'client_secret': 'ab99799453f94d5eba887d7c4a35189e'
     }
