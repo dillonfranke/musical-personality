440e0e00f014f4d97198d8168d49bd0ca10b1d33
Dillon Reece Franke <dfranke@stanford.edu>
2019-12-02 11:44:53 -0800

    adding loading page bug fix4

--- packages/auth.py
+++ packages/auth.py
@@ -13,7 +13,7 @@ bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 @bp.route('/loading')
 def loading():
-    render_template('loading.html')
+    return render_template('auth/loading.html')
 
 
 @bp.route('/link')
